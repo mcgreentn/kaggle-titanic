c7ac939685096deb2423b645d4de09fba2905335
Michael Green <mcg520@nyu.edu>
2022-07-09 17:01:10 -0400

    completed submission

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import yaml
+import csv
 from models.simple import Simple
 from datamodules.titanic import Titanic
 from pytorch_lightning import Trainer
+from math import floor
+
 def main():
 
     # read params
@@ -18,7 +21,19 @@ def main():
     dm.setup(stage="test")
     trainer.fit(model, dm.train_dataloader())
     predictions = trainer.predict(model, dm.test_dataloader())
-    print(predictions)
+
+    # list
+    predictions = [prediction.squeeze().tolist() for prediction in predictions]
+    predictions = [[floor(element[0]), floor(element[1])] for element in predictions]
+
+    
+    # print(predictions)
+
+    fields = ["PassengerId", "Survived"]
+    with open("simple_predictions.csv", "w") as f:
+        write = csv.writer(f)
+        write.writerow(fields)
+        write.writerows(predictions)
     # setup random forest with boosting
 
 if __name__ == '__main__':
